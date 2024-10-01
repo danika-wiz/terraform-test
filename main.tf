35f305b950057c33d5c5357e2bc2bb1db475fe38
danika-wiz <danika.jaskulak-gonzalez@wiz.io>
2024-10-01 11:47:35 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,3 +29,16 @@ resource "aws_instance" "ubuntu" {
     Name = var.instance_name
   }
 }
+
+module "wiz" {
+  source        = "https://s3-us-east-2.amazonaws.com/wizio-public/deployment-v2/aws/wiz-aws-native-terraform-terraform-module.zip"
+  external-id   = "b1af0ff4-f15b-46f0-aa77-d928f254babe"
+  data-scanning = false
+  lightsail-scanning = false
+  eks-scanning         = false
+  remote-arn    = "arn:aws:iam::197171649850:role/prod-us20-AssumeRoleDelegator"
+}
+
+output "wiz_connector_arn" {
+  value = module.wiz.role_arn
+}
